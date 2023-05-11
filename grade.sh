ad9fe182b5618457f87a4eb11d61babf7bc6d4f5
juan1410 <jperezhernandez@ucsd.edu>
2023-05-11 11:49:40 -0700

    changes

--- grade.sh
+++ grade.sh
@@ -8,6 +8,21 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+file='find TestListExamples.java'
+
+cd student-submission
+find ListExamples.java
+
+set -e
+if [[ -f ListExamples.java ]]
+then
+    cp ListExamples.java /Users/juan/Documents/GitHub/list-examples-grader
+    cd ..
+    cp TestListExamples.java ListExamples.java grading-area
+    ls grading-area
+else
+    exit
+fi
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
